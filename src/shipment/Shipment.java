a1a51302b375d54754c0598dc0b48dc694549d20
Azenga Kevin <azenga.kevin7@gmail.com>
2021-01-12 12:30:47 +0300

    Wip, creating and linking the POJOs

--- src/shipment/Shipment.java
+++ src/shipment/Shipment.java
@@ -31,8 +31,10 @@ public class Shipment {
 	private double weight;
 	private Size size;
 	private Distance distance;
+	private static long shipmentCount = 0;
 
 	public Shipment() {
+		assignId();
 	}
 
 	public Shipment(String summary, double weight, Size size, Distance distance) {
@@ -42,16 +44,20 @@ public class Shipment {
 		this.size = size;
 		this.distance = distance;
 		this.orderSaved = false;
-
+		this.status = Status.Unconfirmed;
 		this.assignId();
 	}
 
 	public void assignId() {
 
+		shipmentCount++;
+
+		idNumber = shipmentCount;
+
 	}
 
 	public void saveOrder() {
-
+		this.orderSaved = true;
 	}
 
 	public void updateStatus(Status status) {
@@ -84,6 +90,58 @@ public class Shipment {
 
 	public void setSize(Size size) {
 		this.size = size;
+	}
+
+	public Distance getDistance() {
+		return distance;
+	}
+
+	public void setDistance(Distance distance) {
+		this.distance = distance;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public LocalDateTime getArrivalTime() {
+		return arrivalTime;
+	}
+
+	public void setArrivalTime(LocalDateTime arrivalTime) {
+		this.arrivalTime = arrivalTime;
+	}
+
+	public LocalDateTime getDepartureTime() {
+		return departureTime;
+	}
+
+	public void setDepartureTime(LocalDateTime departureTime) {
+		this.departureTime = departureTime;
+	}
+
+	public LocalDateTime getExpectedDeliveryTime() {
+		return expectedDeliveryTime;
+	}
+
+	public void setExpectedDeliveryTime(LocalDateTime expectedDeliveryTime) {
+		this.expectedDeliveryTime = expectedDeliveryTime;
+	}
+
+	public boolean isOrderSaved() {
+		return orderSaved;
 	}
 
 }
--- src/shipment/ShipmentHistory.java
+++ src/shipment/ShipmentHistory.java
@@ -1,5 +1,57 @@
 package shipment;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import shipment.Shipment.Distance;
+import shipment.Shipment.Size;
+
 public class ShipmentHistory {
 
+	public static List<Shipment> shipments = new ArrayList<>();
+
+	public boolean addItem(String summary, double weight, Size size, Distance distance) {
+
+		return ShipmentHistory.shipments.add(new Shipment(summary, weight, size, distance));
+
+	}
+
+	public boolean removeItem(Shipment shipment) {
+
+		Optional<Shipment> maybeShipment = shipments.parallelStream()
+				.filter(s -> s.getIdNumber() == shipment.getIdNumber()).findFirst();
+
+		if (maybeShipment.isPresent()) {
+			ShipmentHistory.shipments.remove(maybeShipment.get());
+			return true;
+		}
+
+		return false;
+	}
+
+	public Optional<Shipment> retrieveItem(long idNumber) {
+
+		return ShipmentHistory.shipments.stream().filter(s -> s.getIdNumber() == idNumber).findFirst();
+
+	}
+
+	public void updateItem(Shipment shipment, long idNumber) {
+
+		Optional<Shipment> maybeShipment = ShipmentHistory.shipments.stream().filter(s -> s.getIdNumber() == idNumber)
+				.findFirst();
+
+		if (maybeShipment.isPresent()) {
+			Shipment oldShipment = maybeShipment.get();
+
+			oldShipment.setArrivalTime(shipment.getArrivalTime());
+			oldShipment.setDepartureTime(shipment.getDepartureTime());
+			oldShipment.setSize(shipment.getSize());
+			oldShipment.setSummary(shipment.getSummary());
+			oldShipment.setDistance(shipment.getDistance());
+			oldShipment.setWeight(shipment.getWeight());
+			oldShipment.setStatus(shipment.getStatus());
+			oldShipment.setExpectedDeliveryTime(shipment.getExpectedDeliveryTime());
+		}
+	}
 }
